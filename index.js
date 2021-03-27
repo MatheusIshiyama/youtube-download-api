0b2dd5d096bc55aa663c624179ce3b447232d148
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-27 17:00:30 -0300

    finish api

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/info", async (req, res) => {
+    const { url } = req.query;
+
+    const info = (await ytdl.getInfo(url)).videoDetails;
+
+    const title = info.title;
+    const thumbnail = info.thumbnails[2].url;
+
+    res.send({ title: title, thumbnail: thumbnail });
+});
+
+app.get("/mp3", async (req, res) => {
+    const { url } = req.query;
+
+    const videoName = (await ytdl.getInfo(url)).videoDetails.title;
+
+    res.header(
+        "Content-Disposition",
+        `attachment; filename="${videoName}.mp3"`
+    );
+
+    ytdl(url, { format: "mp3" }).pipe(res);
+});
+
+app.get("/mp4", async (req, res) => {
+    const { url } = req.query;
+
+    const videoName = (await ytdl.getInfo(url)).videoDetails.title;
+
+    res.header(
+        "Content-Disposition",
+        `attachment; filename="${videoName}.mp4"`
+    );
+
+    ytdl(url, { format: "mp4" }).pipe(res);
+});
+
+app.listen(process.env.PORT || 3500, () => {
+    console.log("Server on");
+});
