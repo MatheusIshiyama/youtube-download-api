4a2aa5f4f72fe7c46e0399251008a7231a52c685
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-28 16:11:18 -0300

    trying fix download on chrome mobile

--- index.js
+++ index.js
@@ -44,10 +44,11 @@ app.get("/mp3", async (req, res) => {
         if (isValid) {
             const videoName = (await ytdl.getInfo(url)).videoDetails.title;
 
-            res.header(
+            res.setHeader(
                 "Content-Disposition",
                 `attachment; filename="${videoName}.mp3"`
             );
+            res.setHeader("Content-type", "audio/mpeg3");
 
             ytdl(url, { quality: "highestaudio", format: "mp3" }).pipe(res);
         } else {
